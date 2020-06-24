677f6ff8d98e372b709751e523ef99ef1d060521
Nguyen Marc <nguyen_marc@live.fr>
2020-06-24 19:10:35 +0200

    [actualite] Better visuals

---
+++ lib/presentation/widgets/animations/marquee.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MarqueeWidget extends StatefulWidget {
+  final Widget child;
+  final Axis direction;
+  final Duration animationDuration, backDuration, pauseDuration;
+
+  const MarqueeWidget({
+    @required this.child,
+    this.direction = Axis.horizontal,
+    this.animationDuration = const Duration(milliseconds: 3000),
+    this.backDuration = const Duration(milliseconds: 800),
+    this.pauseDuration = const Duration(milliseconds: 800),
+  });
+
+  @override
+  _MarqueeWidgetState createState() => _MarqueeWidgetState();
+}
+
+class _MarqueeWidgetState extends State<MarqueeWidget> {
+  ScrollController scrollController;
+
+  @override
+  void initState() {
+    scrollController = ScrollController(initialScrollOffset: 50.0);
+    WidgetsBinding.instance.addPostFrameCallback(scroll);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: widget.direction,
+      controller: scrollController,
+      child: widget.child,
+    );
+  }
+
+  Future<void> scroll(dynamic _) async {
+    while (scrollController.hasClients) {
+      await Future<void>.delayed(widget.pauseDuration);
+      if (scrollController.hasClients) {
+        await scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: widget.animationDuration,
+          curve: Curves.linear,
+        );
+      }
+      await Future<void>.delayed(widget.pauseDuration);
+      if (scrollController.hasClients) {
+        await scrollController.animateTo(
+          0.0,
+          duration: widget.backDuration,
+          curve: Curves.linear,
+        );
+      }
+    }
+  }
+}
